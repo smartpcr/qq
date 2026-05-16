1d8ad6e09954a4e70e4abf7cc531471353f03451
Forge Architect <forge-architect@local>
2026-05-15 19:04:55 -0700

    fix(ws-qq-slack-messenger-supp-phase-solution-scaffolding-stage-prerequisite-abstraction-compile-stubs): address review comment

--- src/AgentSwarm.Messaging.Abstractions/AgentQuestion.cs
+++ src/AgentSwarm.Messaging.Abstractions/AgentQuestion.cs
@@ -1,3 +1,9 @@
+// -----------------------------------------------------------------------
+// <copyright file="AgentQuestion.cs" company="Microsoft Corp.">
+//     Copyright (c) Microsoft Corp. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
 namespace AgentSwarm.Messaging.Abstractions;
 
 /// <summary>
